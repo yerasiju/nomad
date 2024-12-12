ab0a2733f5f6b399698e2f64f01f10ed2f2916ec
yerasiju <144224665+yerasiju@users.noreply.github.com>
2024-12-12 13:27:17 +0500

    nomad

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,30 @@ import Reserve from "../../components/Reservation/Reserve";
 
 const Home = () => {
   const dishes = [
-    { id: 1, name: "Beshbarmak", image: "/public/images/beshbarmakk.jpg" },
-    { id: 2, name: "Kurt", image: "/public/images/kuyrdakk.jpg" },
-    { id: 3, name: "Plov", image: "/public/images/plovv.jpg" },
+    { id: 1, name: "Beshbarmak", image: "/nomad/images/beshbarmakk.jpg" },
+    { id: 2, name: "Kurt", image: "/nomad/images/kuyrdakk.jpg" },
+    { id: 3, name: "Plov", image: "/nomad/images/plovv.jpg" },
+  ];
+
+  const aboutSections = [
+    {
+      title: "Our Vision",
+      text: "To bring the rich flavors of nomadic cuisine to the world, offering an authentic dining experience that connects people to the traditions and heritage of Kazakhstan.",
+      image: "/nomad/images/shanrak.jpg",
+      alt: "Vision",
+    },
+    {
+      title: "Our Mission",
+      text: "To provide a memorable dining experience by serving traditional Kazakh dishes made from locally sourced ingredients, while educating our guests about the history and culture behind each dish.",
+      image: "/nomad/images/tu.jpg",
+      alt: "Mission",
+    },
+    {
+      title: "Our Philosophy",
+      text: "We believe in the power of food to connect people across cultures and generations. Our dishes are inspired by the nomadic traditions of Kazakhstan, with a focus on authenticity, quality, and sustainability.",
+      image: "/nomad/images/dombyra.jpg",
+      alt: "Philosophy",
+    },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -47,6 +68,12 @@ const Home = () => {
       </div>
 
       <div className={styles.introSection}>
+        <img
+          src="https://images.pexels.com/photos/4588816/pexels-photo-4588816.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          className={styles.sectionImage}
+        ></img>
+
         <h2 className={styles.introTitle}>Introduction to Kazakh Cuisine</h2>
         <div className={styles.introContent}>
           <p className={styles.introText}>
@@ -63,6 +90,21 @@ const Home = () => {
             snack, are central to Kazakh culinary identity.
           </p>
         </div>
+      </div>
+
+      <div className={styles.aboutContainer}>
+        <h1 className={styles.title}>About Nomad</h1>
+        {aboutSections.map((section, index) => (
+          <div key={index} className={styles.aboutItem}>
+            <div className={styles.aboutImage}>
+              <img src={section.image} alt={section.alt} />
+            </div>
+            <div className={styles.aboutText}>
+              <h3>{section.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html: section.text }}></p>
+            </div>
+          </div>
+        ))}
       </div>
 
       <Reserve></Reserve>
